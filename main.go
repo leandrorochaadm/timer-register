2b44019bb5576d5c1364c2a8c5900d62bd21a2a7
leandro <leandro@eulabs.com.br>
2021-09-26 12:31:33 -0400

    desativando rotas e banco

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/leandrorochaadm/time-register/config"
-	"github.com/leandrorochaadm/time-register/database"
 	date_deadline "github.com/leandrorochaadm/time-register/date-deadline"
-	"github.com/leandrorochaadm/time-register/router"
 )
 
 func main() {
-	config.Load()
-	database.InitialiseDBConnection()
-	router.Generate()
+	//config.Load()
+	//database.InitialiseDBConnection()
+	//router.Generate()
 
-	fmt.Println("API executando na porta ", config.Port)
+	//fmt.Println("API executando na porta ", config.Port)
 
+	fmt.Println("\n-------------------------------------------------------------------------------------")
 	date_deadline.DeadlineYear()
 	date_deadline.DeadlineLife()
-	fmt.Println("---------")
+	fmt.Println("\n-------------------------------------------------------------------------------------")
 	//fmt.Println(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC).Add(time.Hour*24*(365/2)))
 
 }
